a04e9b0907c7a131a89236ea21acedf2eb551c99
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-07-18 21:51:57 +0300

    task 1 end

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,10 @@
 const catigList = document.querySelector(`#categories`);
 const catigItem = catigList.querySelectorAll(`.item`);
 
+const titleAll = document.querySelectorAll(`.item > h2`);
+const ulAll = document.querySelectorAll(`.item ul`);
+const liAll = document.querySelectorAll(`.item li`);
+
 console.log(`Number of categories: ${catigItem.length}`);
 
 catigItem.forEach(item => {
@@ -8,8 +12,40 @@ catigItem.forEach(item => {
 
     const elemAll = item.querySelectorAll('ul li').length;
 
-
-
     console.log(`Category: ${catigName}`);
     console.log(`Elements: ${elemAll}`);
 });
+
+catigList.classList.add(`js-catigories`);
+
+catigItem.forEach((item) => {
+  item.classList.add(`catigitem`);
+});
+
+titleAll.forEach((item) => {
+  item.classList.add(`js-htwoelem`);
+});
+
+ulAll.forEach((item) => {
+  item.classList.add(`js-ulelem`);
+});
+
+liAll.forEach((item) => {
+  item.classList.add(`js-lielem`);
+});
+
+
+
+
+
+
+
+// catigItem.forEach((item) => {
+//   item.classList.add("js-li-item");
+// });
+
+
+
+// liAll.forEach((item) => {
+//   item.classList.add("js-lielem");
+// });
